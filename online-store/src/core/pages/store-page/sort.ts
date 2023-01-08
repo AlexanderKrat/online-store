106a6f218f278d3c561045416f76704455f594bf
Rikki Monti <110960926+RikoMani84@users.noreply.github.com>
2023-01-08 23:58:41 +0500

    refactor: change sort.ts file

--- online-store/src/core/pages/store-page/sort.ts
+++ online-store/src/core/pages/store-page/sort.ts
@@ -1,79 +1,16 @@
-import { SearchField } from './search';
 import { SortByEnum, SortersInterface } from './../../components/types';
 import { Filters } from "./filter";
+import { SearchField } from './search';
 
-export class Sorters extends Filters implements SortersInterface {
+export class Sorters extends Filters implements SortersInterface{
     static activeSortBtn: HTMLButtonElement | null;
 
     checkLocalStorageSorter(sorterVal = ''): void {
-        const currentSorters: string | null = localStorage.getItem('sorter');
-        if (currentSorters !== sorterVal && currentSorters) {
+        const currentSorters: string|null=localStorage.get('sorter');
+        if(currentSorters!==sorterVal&& currentSorters){
             localStorage.setItem('sorter', sorterVal)
-        } else if (!currentSorters) [
+        } else if(!currentSorters){
             localStorage.setItem('sorter', sorterVal)
-        ]
-    }
-    createSorters(): HTMLElement {
-        const sort_bar = document.createElement("div");
-        const sortByNameAscendingBtn: HTMLButtonElement = this.createSortersBtns(
-            SortByEnum.NameAsc,
-            'A-z'
-        );
-        const sortByNameDecendingBtn: HTMLButtonElement = this.createSortersBtns(
-            SortByEnum.NameDes,
-            'z-A'
-        );
-        const sortByPriceAscendingBtn: HTMLButtonElement = this.createSortersBtns(
-            SortByEnum.PriceAsc,
-            'Higher Price'
-        );
-        const sortByPriceDecendingBtn: HTMLButtonElement = this.createSortersBtns(
-            SortByEnum.PriceDes,
-            'Lower Price'
-        );
-        sort_bar.append(
-            sortByNameAscendingBtn,
-            sortByNameDecendingBtn,
-            sortByPriceAscendingBtn,
-            sortByPriceDecendingBtn
-        )
-        this.checkForActiveSortersOnReload();
-        return sort_bar;
-    }
-    createSortersBtns(sorterName: string, innerText: string): HTMLButtonElement {
-        const sortBtn: HTMLButtonElement = document.createElement('button');
-        sortBtn.className = "btn btn-secondary me-2";
-        sortBtn.setAttribute('sorter', sorterName);
-        sortBtn.innerText = innerText;
-
-        sortBtn.addEventListener('click', (): void => {
-            this.checkLocalStorageSorter(sorterName);
-            Sorters.sortBy(sorterName);
-            this.checkForActiveSorterBtn(sortBtn);
-        });
-        return sortBtn;
-    }
-    checkForActiveSorterBtn(btn: HTMLButtonElement): void {
-        if (Sorters.activeSortBtn) {
-            Sorters.activeSortBtn.classList.toggle('sorters__btn--active');
-            Sorters.activeSortBtn = btn;
-            btn.classList.toggle('sorters__btn--active');
-        } else {
-            btn.classList.toggle('sorters__btn--active');
-            Sorters.activeSortBtn = btn;
-        }
-    }
-    checkForActiveSortersOnReload(): void {
-        const activeSorters: string | null = localStorage.getItem('sorter');
-        const sorterBtns: HTMLCollection = document.getElementsByClassName('btn btn-secondary me-2');
-        if (activeSorters) {
-            for (const btn of sorterBtns) {
-                if (btn.getAttribute('sorter') === activeSorters) {
-                    btn.classList.add('sorters__btn--active');
-                    Sorters.activeSortBtn = btn as HTMLButtonElement;
-                    return;
-                }
-            }
         }
     }
     static sortBy(activeSorter: string) {
@@ -100,4 +37,18 @@ export class Sorters extends Filters implements SortersInterface {
         const createProductsItems = new Storage();
         createProductsItems.createProductsItems(Sorters.currentCollection); 
     }
+    createSorters(): HTMLElement {
+        throw new Error('Method not implemented.');
+    }
+    createSortersBtns(sorterName: string, innerText: string): HTMLButtonElement {
+        throw new Error('Method not implemented.');
+    }
+    checkForActiveSorterBtn(btn: HTMLButtonElement): void {
+        throw new Error('Method not implemented.');
+    }
+    checkForActiveSortersOnReload(): void {
+        throw new Error('Method not implemented.');
+    }
+
 }
+
